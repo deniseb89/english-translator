9d3032d877063a6f7c961207dcfad0911bd302da
deniseb89 <denise_b89@hotmail.com>
2016-04-22 19:52:52 -0400

    clean up spacing

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,13 +1,15 @@
 var app = angular.module('translator', []);
 
 app.controller('WordDisplay', ['$scope', '$http', function($scope, $http){
-  $http.get('http://english-english-api.herokuapp.com/api/words').success(function(data, status, headers, config) {
-    $scope.wordList = data;
-  });
+  $http.get('http://english-english-api.herokuapp.com/api/words')
+       .success(function(data, status, headers, config) {
+          $scope.wordList = data;
+       });
 
   $scope.search = function(query) {
-    $http.get('http://english-english-api.herokuapp.com/api/words/' + query).success(function(data, status, headers, config) {
-      $scope.wordList = data;
-    });
+    $http.get('http://english-english-api.herokuapp.com/api/words/' + query)
+         .success(function(data, status, headers, config) {
+            $scope.wordList = data;
+         });
   }
 }]);
