e7446cd2223a9df675db9a3ba95437e14b7e8678
deniseb89 <denise_b89@hotmail.com>
2016-04-21 21:40:46 -0400

    whoops

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,11 +2,11 @@ var app = angular.module('translator', []);
 
 app.controller('WordDisplay', ['$scope', '$http', function($scope, $http){
   $http.get('http://english-english-api.herokuapp.com/api/words').success(function(data, status, headers, config) {
-    $scope.words = data;
+    $scope.wordList = data;
   });
 
 }]);
 
 app.controller('SearchController', function(){
-  
+
 });
