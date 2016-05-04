d1e901879295126aa65fd2ef0eb8e57751f1d73b
Penhead <penheadcreative@gmail.com>
2016-05-03 21:52:12 -0400

    style updates and controller data changes

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,10 +3,10 @@ var app = angular.module('translator', ['ngCookies']);
 app.controller('WordDisplay', ['$scope', '$http', function($scope, $http){
 	$scope.hasResults = false;
 
-  $http.get('http://english-english-api.herokuapp.com/api/words')
-       .success(function(data, status, headers, config) {
-          $scope.wordList = data;
-       });
+  // $http.get('http://english-english-api.herokuapp.com/api/words')
+  //      .success(function(data, status, headers, config) {
+  //         $scope.wordList = data;
+  //      });
 
   $scope.search = function(query) {
 	  if(query != null){
@@ -16,7 +16,9 @@ app.controller('WordDisplay', ['$scope', '$http', function($scope, $http){
 			 console.log(data);
 			if(data.length != 0){
 				$scope.wordList = data;
-			}
+			}else{
+        $scope.message = true;
+      }
      }); 
 	  }
   }
@@ -37,7 +39,9 @@ app.controller('WordDisplay', ['$scope', '$http', function($scope, $http){
 app.controller('OriginCountry', ['$scope', '$cookies', function($scope, $cookies){
   if($cookies.get('country')) {
     $scope.ask = true;
+    
   }
+  $scope.country = $cookies.get("country");
   $scope.default = function(country) {
     var expiration = new Date();
     expiration.setDate(expiration.getDate() + 90);
